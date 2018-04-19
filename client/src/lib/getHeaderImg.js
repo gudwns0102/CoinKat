435e334ca0e8b8c4646f5c51177f35c6bb7d9c7b
gudwns0102 <gudwns0102@postech.ac.kr>
2018-04-19 12:04:14 +0900

    a

--- client/src/lib/getHeaderImg.js
+++ client/src/lib/getHeaderImg.js
@@ -1,83 +1,26 @@
 export default getHeaderImage = (name) => {
-    
-  switch(name){
-      case 'BTC':{
-          return require('../../assets/images/coin/BTC.png');
-      }
 
-      case 'ETH':{
-          return require('../../assets/images/coin/ETH.png');
-      }
-
-      case 'XRP':{
-          return require('../../assets/images/coin/XRP.png');
-      }
-
-      case 'QTUM':{
-          return require('../../assets/images/coin/QTUM.png');
-      }
-
-      case 'BTG':{
-          return require('../../assets/images/coin/BTG.png');
-      }
-
-      case 'EOS':{
-          return require('../../assets/images/coin/EOS.png');
-      }
-
-      case 'XMR':{
-          return require('../../assets/images/coin/XMR.png');
-      }
-
-      case 'ZEC':{
-          return require('../../assets/images/coin/ZEC.png');
-      }
-
-      case 'DASH':{
-          return require('../../assets/images/coin/DASH.png');
-      }
-
-      case 'LTC':{
-          return require('../../assets/images/coin/LTC.png');
-      }
-
-      case 'BCH':{
-          return require('../../assets/images/coin/BCH.png');
-      }
-
-      case 'ETC':{
-          return require('../../assets/images/coin/ETC.png');
-      }
-        
-      case 'IOTA':{
-          return require('../../assets/images/coin/IOTA.png');
-      }
-
-      case 'ICX': {
-          return require('../../assets/images/coin/ICX.png')
-      }
-
-      case 'VEN': {
-        return require('../../assets/images/coin/VEN.png')
-      }
-
-      case 'TRX': {
-        return require('../../assets/images/coin/TRX.png')
-      }
-
-      case 'ELF': {
-        return require('../../assets/images/coin/ELF.png')
-      }
-
-      case 'MITH': {
-        return require('../../assets/images/coin/MITH.png')
-      }
-
-      case 'OMG': {
-        return require('../../assets/images/coin/OMG.png')
-      }
-
-      default:
-          return;
+  var name2img = {
+       'BTC': require('../../assets/images/coin/BTC.png'),
+       'ETH': require('../../assets/images/coin/ETH.png'),
+       'XRP': require('../../assets/images/coin/XRP.png'),    
+       'QTUM':require('../../assets/images/coin/QTUM.png'),
+       'BTG': require('../../assets/images/coin/BTG.png'),
+       'EOS': require('../../assets/images/coin/EOS.png'),
+       'XMR': require('../../assets/images/coin/XMR.png'),
+       'ZEC': require('../../assets/images/coin/ZEC.png'),
+       'DASH':require('../../assets/images/coin/DASH.png'),
+       'LTC': require('../../assets/images/coin/LTC.png'),
+       'BCH': require('../../assets/images/coin/BCH.png'),      
+       'ETC': require('../../assets/images/coin/ETC.png'),              
+       'IOTA':require('../../assets/images/coin/IOTA.png'),      
+       'ICX': require('../../assets/images/coin/ICX.png'),     
+       'VEN': require('../../assets/images/coin/VEN.png'),     
+       'TRX': require('../../assets/images/coin/TRX.png'),    
+       'ELF': require('../../assets/images/coin/ELF.png'),      
+       'MITH':require('../../assets/images/coin/MITH.png'),    
+       'OMG': require('../../assets/images/coin/OMG.png'),
   }
+ 
+  return name2img[name];
 }
--- server/index.js
+++ server/index.js
@@ -8,6 +8,18 @@ var api = new ParseServer({
   appId: 'QWDUKSHKDWOP@coinkat$HOFNDSESL#L',
   fileKey: 'myFileKey',
   masterKey: 'AOWHDDKNKLLR$@303k',
+  push: {
+    android: {
+      senderId: '1519459543887',
+      apiKey: '	AIzaSyBsEiJH9M_FUwHkdtW0ptd9DbDVw87bShY',
+    },
+    ios: {
+      pfx: '/file/path/to/XXX.p12',
+      passphrase: '', // optional password to your p12/PFX
+      bundleId: '',
+      production: false
+    }
+  }
 });
 
 // Serve the Parse API at /parse URL prefix
