9c57b7ee58e342cbc8b8cfd7ec04d2aa57039bb1
gudwns0102 <gudwns0102@postech.ac.kr>
2018-04-20 14:31:44 +0900

    c

--- server/PushManager.js
+++ server/PushManager.js
@@ -1,57 +1,26 @@
 var FCM = require('fcm-node');
-var mongoose = require('mongoose');
+var Parse = require('parse/node');
 var FCM_KEY= 'AAAAcmJLzBY:APA91bF2x1IhD0HipgY7MY3ovle_fkizJEXJvK8s2kEAP-JPBa31i2zViSAT3OOD3EN84r4MoHat_2llwXiI67y7VkR760oSoSyzcucptu6VRaLY_lJTTYAXQE3Rjp43H_5empiNyjWj'
 
 
 class PushManager{
   constructor(){
     this.fcm = new FCM(FCM_KEY);
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/CoinKat')
-    .then(() => console.log('MongoDB connection Success'))
-    .catch(e => console.log(e));
+   
+    var Push = Parse.Object.extend("Push");
+    var query = new Parse.Query(Push);
 
-    
-    const pushSchema = new mongoose.Schema({
-      exchange: {type: String, required: true},
-      name: {type: String, required: true},
-      upPrice: {type: Number, required: true},
-      downPrice: {type: Number, required: true},
-      _p_parent: {type: Object, required: true}
-    },
-    {
-      collection: 'Push',
-      timeStamp: true
-    });
-
-    const userSchema = new mongoose.Schema({
-      _id: {type: String, required: true, unique: true}
-    },{
-      collection: 'User'
+    query.find({
+      success: results => {
+        console.log(results)
+        res.success({code: 100});
+      },
+      error: (err) => {
+        console.log(err)
+        res.error();
+      }
     })
-
-    pushSchema.statics.findAll = function(){
-      return this.find({});
-    }
-
-    userSchema.statics.findById = function(_id){
-      return this.find({_id});
-    }
-
-    this.Push = mongoose.model('Push', pushSchema);
-    this.User = mongoose.model('User', userSchema);
-
-    this.run();
-  }
-
-  async run(){
-    var pushes = await this.Push.findAll();
     
-    var userId = pushes.split('$')[1];
-
-    var user = await this.User.findById(userId);
-
-    console.log(user);
   }
 
 }
