97e946eb117b80c17bf8f3b4f2e258bed297c2fe
gudwns0102 <gudwns0102@postech.ac.kr>
2018-04-27 15:24:20 +0900

    c

--- server/index.js
+++ server/index.js
@@ -14,45 +14,17 @@ var Parse = require('parse/node');
 
 var PushManager = require('./PushManager');
 var pushManager = new PushManager();
-/*
-var myKey = 'AAAAcmJLzBY:APA91bF2x1IhD0HipgY7MY3ovle_fkizJEXJvK8s2kEAP-JPBa31i2zViSAT3OOD3EN84r4MoHat_2llwXiI67y7VkR760oSoSyzcucptu6VRaLY_lJTTYAXQE3Rjp43H_5empiNyjWj'
-var token = 'f2Hz1j_afP0:APA91bHzgD9GReXajQdJuC8l87NbFmUxNIOXwxWpEwzwurHPYNj2fWoSDVj4bnNKxMrfcR5UQU048f7y9uC9eKExm1Z7OK6qeWxCGL8-DV7rXHjx1ZkNhpnHOgkJNQqgzP089LUqE4P9'
 
-var FCM = require('fcm-node');
-var fcm = new FCM(myKey);
+var cors = require('cors')
 
-var push_data = {
-  to: token,
-  notification: {
-    title: "Hello Node",
-    body: "Node로 발송하는 Push 메시지 입니다.",
-    sound: "default",
-    click_action: "FCM_PLUGIN_ACTIVITY",
-    icon: "fcm_push_icon"
-  },
-  priority: "high",
-  // App 패키지 이름
-  restricted_package_name: "com.client",
-  // App에게 전달할 데이터
-  data: {
-      num1: 2000,
-      num2: 3000
-  }
-}
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
-fcm.send(push_data, function(err, response) {
-  if (err) {
-      console.error('Push메시지 발송에 실패했습니다.');
-      console.error(err);
-      return;
-  }
-
-  console.log('Push메시지가 발송되었습니다.');
-  console.log(response);
-});*/
-
-// Serve the Parse API at /parse URL prefix
 app.use('/parse', api);
+app.use(cors());
 
 app.get('/all', (req, res) => {
   res.send(ticker.data);
